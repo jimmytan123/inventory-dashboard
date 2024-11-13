36ac58ac1d6b5613e93c2c9ce502d9ff9910a0bd
Jimmy Tan <jimmytan0424@gmail.com>
2024-11-12 16:00:42 -0800

    Update chart height

--- client/src/app/dashboard/CardPurchaseSummary.tsx
+++ client/src/app/dashboard/CardPurchaseSummary.tsx
@@ -57,7 +57,7 @@ const CardPurchaseSummary = () => {
                 )}
               </div>
             </div>
-            <ResponsiveContainer width="100%" height={190} className="p-2">
+            <ResponsiveContainer width="100%" height={180} className="p-2">
               <AreaChart
                 data={purchaseData}
                 margin={{ top: 0, right: 0, left: -50, bottom: 45 }}
